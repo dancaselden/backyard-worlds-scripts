19fb0df09e8f0bd875f1e0d26f0deb322337ed30
Dan Caselden <dancaselden@gmail.com>
2017-04-29 03:03:30 +0000

    Worst code in the world for combining W1&W2

--- byw/www/unoverse.py
+++ byw/www/unoverse.py
@@ -34,6 +34,63 @@ GRAD = [
     "gradient:red-black",
 ]
 
+def get_cutouts(ra, dec, size, band, version, brighten, equalize):
+    images = []
+    for band in (1,2):
+        # Construct URL to cutout and download
+        url = PATH.format(ra=ra, dec=dec, size=size, band=band, version=version)
+        print url
+        response = requests.get(url)
+        if response.status_code != 200:
+            return ("Request failed", 500)
+
+        # Extract fits from tar.gz
+        targz = tarfile.open(fileobj=StringIO(response.content), mode="r:gz")
+        cutouts = []
+        for member in targz.getmembers():
+            print member.name
+            if member.name.endswith(".fits"):
+                cutout = targz.extractfile(member)
+                cutouts.append(cutout)
+
+        if not cutouts:
+            return ("Failed to extract fits file from response", 500)
+
+        # Equalize or no?
+        eql = "-equalize" if equalize else ""
+        outfs = []
+        for offset, cutout in enumerate(cutouts):
+            if (offset + 1) == len(cutouts):
+                command = "convert {inf} {eql} -scale 500% {outf}"
+            else:
+                command = "convert {inf} {eql} -scale 500% -background black -gravity East -splice 5x0+0+0 {outf}"
+
+            # Write fits file to disk
+            inf = NamedTemporaryFile(suffix=".fits")
+            inf.write(cutout.read())
+            inf.seek(0)
+
+            outf = NamedTemporaryFile(suffix=".png")
+            outfs.append(outf)
+
+            # Convert img with imagemagick
+            subprocess.check_output(command.format(inf=inf.name, outf=outf.name, eql=eql), shell=True)
+
+        # Stitch images together
+        images.append(subprocess.check_output("convert -background black {0} +append -".format(" ".join([outf.name for outf in outfs])), shell=True))
+
+    infs = []
+    for image in images:
+        inf = NamedTemporaryFile(suffix=".png")
+        inf.write(image)
+        inf.seek(0)
+        infs.append(inf)
+    outf = NamedTemporaryFile(suffix=".png")
+    cmd = "convert {inf2} {inf1} -background black -channel RB -combine -".format(inf1=infs[0].name,inf2=infs[1].name)
+    image = subprocess.check_output(cmd,shell=True)
+
+    return StringIO(image), response.status_code
+
 def get_cutout(ra, dec, size, band, version, brighten, equalize):
     # Construct URL to cutout and download
     url = PATH.format(ra=ra, dec=dec, size=size, band=band, version=version)
@@ -94,7 +151,7 @@ class Convert(Resource):
         parser.add_argument("ra", type=float, required=True)
         parser.add_argument("dec", type=float, required=True)
         parser.add_argument("size", type=int, default=100)
-        parser.add_argument("band", type=int, default=2, choices=[1,2])
+        parser.add_argument("band", type=int, default=3, choices=[1,2,3])
         parser.add_argument("version", type=str, default="neo2", 
                                     choices=["allwise", "neo1", "neo2"])
         parser.add_argument("brighten", type=int, default=0,
@@ -103,9 +160,12 @@ class Convert(Resource):
                             default=True)
         args = parser.parse_args()
 
-        cutout, status = get_cutout(**args)
-        if status != 200:
-            return "Request failed", 500
+        if args.band in (1,2):
+            cutout, status = get_cutout(**args)
+        else:
+            cutout, status = get_cutouts(**args)
+            if status != 200:
+                return "Request failed", 500
 
         return send_file(cutout, mimetype="image/png")   
 
